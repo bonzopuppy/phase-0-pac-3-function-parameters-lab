9f32cae1931cc0ad096f9185455b663d0e7d733e
David Baden <davebaden@gmail.com>
2023-08-16 08:56:24 -0700

    updated assignment

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-function introduction (name) {
-    console.log(`Hi, my name is ${name}.`);
+function introduction(name) {
+    return (`Hi, my name is ${name}.`);
 }
 
-function introductionWithlanguage(name, language) {
-    console.log(`Hi, my name is ${name} and I am learning to program in ${language}.`);
+function introductionWithLanguage(name, language) {
+    return (`Hi, my name is ${name} and I am learning to program in ${language}.`);
 }
 
-function introductionWithlanguageOptional(name, language = "JavaScript") {
-    console.log(`Hi, my name is ${name} and I am learning to program in ${language}.`);
+function introductionWithLanguageOptional(name, language = "JavaScript") {
+    return (`Hi, my name is ${name} and I am learning to program in ${language}.`);
 }
 
